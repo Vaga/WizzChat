47e5be06efa244b998b57bd8d0519f37e8954b65
Vaga <fabien.casters@gmail.com>
2016-02-24 17:42:21 +0100

    Added QRCode system

--- app/src/main/java/vaga/io/wizzchat/AddActivity.java
+++ app/src/main/java/vaga/io/wizzchat/AddActivity.java
@@ -1,10 +1,26 @@
 package vaga.io.wizzchat;
 
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.List;
 
 public class AddActivity extends AppCompatActivity {
+
+    private static final String TAG = "AddActivity";
+
+    private EditText _publicKeyEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,10 +28,69 @@ public class AddActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
 
+        _publicKeyEditText = (EditText) findViewById(R.id.publicKeyEditText);
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("New contact");
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void onScan(View view) {
+
+        try {
+            Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+            intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+            startActivityForResult(intent, 0);
+        } catch (ActivityNotFoundException anfe) {
+            showDialog(AddActivity.this, "No Scanner Found", "Download a scanner code activity?", "Yes", "No").show();
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+        if (requestCode == 0) {
+
+            if (resultCode == RESULT_OK) {
+
+                String contents = intent.getStringExtra("SCAN_RESULT");
+                String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+                Log.d(TAG, format);
+                Log.d(TAG, contents);
+
+                _publicKeyEditText.setText(contents);
+
+            } else if (resultCode == RESULT_CANCELED) {
+                // Handle cancel
+            }
+        }
+    }
+
+    private static AlertDialog showDialog(final Activity act, CharSequence title, CharSequence message, CharSequence buttonYes, CharSequence buttonNo) {
+
+        AlertDialog.Builder downloadDialog = new AlertDialog.Builder(act);
+        downloadDialog.setTitle(title);
+        downloadDialog.setMessage(message);
+
+        downloadDialog.setPositiveButton(buttonYes, new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                Uri uri = Uri.parse("market://search?q=pname:" + "com.google.zxing.client.android");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+
+                try {
+                    act.startActivity(intent);
+                } catch (ActivityNotFoundException anfe) {}
+            }
+        });
+
+        downloadDialog.setNegativeButton(buttonNo, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialogInterface, int i) {}
+        });
+
+        return downloadDialog.show();
     }
 }
--- app/src/main/java/vaga/io/wizzchat/GetActivity.java
+++ app/src/main/java/vaga/io/wizzchat/GetActivity.java
@@ -1,10 +1,27 @@
 package vaga.io.wizzchat;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import io.realm.Realm;
+import vaga.io.wizzchat.models.Profile;
 
 public class GetActivity extends AppCompatActivity {
+
+    private TextView _nameTextView;
+    private TextView _emailTextView;
+    private ImageView _qrcodeImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,10 +29,53 @@ public class GetActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get);
 
+        _nameTextView = (TextView) findViewById(R.id.nameTextView);
+        _emailTextView = (TextView) findViewById(R.id.emailTextView);
+        _qrcodeImageView = (ImageView) findViewById(R.id.qrcodeImageView);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("My identity");
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Realm realm = Realm.getInstance(GetActivity.this.getApplicationContext());
+
+        // Get the profile
+        Profile profile = realm.where(Profile.class).findFirst();
+        if (profile == null) {
+            Toast.makeText(getBaseContext(), "Internal error", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        // Hydrate the view
+        _nameTextView.setText(profile.getName());
+        _emailTextView.setText(profile.getEmail());
+
+        drawQRCode(profile);
+    }
+
+    // TODO : Better way to retrieve the QRCode
+    private void drawQRCode(Profile profile) {
+
+        String url = "https://api.qrserver.com/v1/create-qr-code/?size=800x800&data=";
+        url += profile.getPublicKey();
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        try {
+            URL urlConnection = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) urlConnection.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+
+            _qrcodeImageView.setImageBitmap(myBitmap);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
